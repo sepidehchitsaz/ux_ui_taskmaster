4d9ef3d158487014b8b417f28dc1bf22bfaad9da
sepidehchitsaz <70854585+sepidehchitsaz@users.noreply.github.com>
2021-04-20 18:09:29 -0400

    ux_ui_taskmaster

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
-// const toggleButton = document.getElementsByClassName('toggle-button')[0]
-// const navbarLinks = document.getElementsByName('navbar-links')[0]
 
-// toggleButton.addEventListener('click', () => {
-//     navbarLinks.classList.toggle('active')
-// })
 
 $(".toggle-button").click(function(){
     $(".navbar-links").toggleClass("active");
